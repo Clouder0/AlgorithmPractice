7f8faae5b356283c9d9cb23e013ac578278dd646
mcxiguajun@outlook.com <mcxiguajun@outlook.com>
2020-02-07 14:00:12 +0800

    ModifiedFolderName

---
+++ ParticipantedContests/LGR-068/A.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+template<typename T>
+void read(T &r)
+{
+    static char c;r=0;
+    for(c=getchar();c>'9'||c<'0';c=getchar());
+    for(;c>='0'&&c<='9';r=(r<<1)+(r<<3)+(c^48),c=getchar());
+}
+int n;
+int main()
+{
+    read(n);
+
+    return 0;
+}

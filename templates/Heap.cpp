5e0ebab9f14ae4189e8d3cf93af5af6b778a801a
Clouder0 <clouder0@outlook.com>
2023-03-04 00:11:53 +0800

    add heap

--- templates/Heap.cpp
+++ templates/Heap.cpp
@@ -1,59 +1,116 @@
+#include <algorithm>
 #include <cstdio>
-template<typename T>
-inline void swap(T &a,T &b)
-{
-    T t = a;
-    a = b;
-    b = t;
-}
-template<typename T>
-inline T read(T &r)
-{
-    static char c;r=0;
-    for(c=getchar();c>'9'||c<'0';c=getchar());
-    for(;c>='0'&&c<='9';r=(r<<1)+(r<<3)+(c^48),c=getchar());
-    return r;
-}
 const int maxn = 5e6 + 10;
-int n,m;
-int a[maxn];
-int size;
-inline void up(int x){while(x > 1 && a[x] < a[x>>1]) swap(a[x],a[x>>1]),x >>= 1;}
-inline void down(int x)
+// small top
+template <typename T>
+struct smallheap
 {
-    while((x << 1) <= size)
+    T a[maxn];
+    int sz;
+    void up(int x)
     {
-        int t = x << 1;
-        if(t < size && a[t + 1] < a[t]) ++t; //选择儿子中较小的替换父亲以维持性质
-        if(a[t] >= a[x]) break;
-        swap(a[x],a[t]);
-        x = t;
+        while (x > 1 && a[x] < a[x >> 1])
+        {
+            std::swap(a[x], a[x >> 1]);
+            x >>= 1;
+        }
+    }
+    void down(int x)
+    {
+        while ((x << 1) <= sz)
+        {
+            int to = x << 1;
+            if (to + 1 <= sz && a[to + 1] < a[to]) ++to;  // if rs is smaller, swap to rs
+            if (a[x] <= a[to]) break;                      // down violates the heap property
+            std::swap(a[x], a[to]);
+            x = to;
+        }
+    }
+    void push(T x) { a[++sz] = x, up(sz); }
+    void pop() { a[1] = a[sz--], down(1); }
+    T top() { return a[1]; }
+    void build(int n)
+    {
+        sz = n;
+        for (int i = (n >> 1); i; --i) down(i);
+    }
+    void build(T* b, int n)
+    {
+        for (int i = 1; i <= n; ++i) a[i] = b[i];
+        build(n);
+    }
+    bool empty() { return !sz; }
+    void clear() { sz = 0; }
+};
+
+// large top
+template <typename T>
+struct largeheap
+{
+    T a[maxn];
+    int sz;
+    void up(int x)
+    {
+        while (x > 1 && a[x] > a[x >> 1])
+        {
+            std::swap(a[x], a[x >> 1]);
+            x >>= 1;
+        }
+    }
+    void down(int x)
+    {
+        while ((x << 1) <= sz)
+        {
+            int to = x << 1;
+            if (to + 1 <= sz && a[to + 1] > a[to]) ++to;  // if rs is larger, swap to rs
+            if (a[x] >= a[to]) break;                      // down violates the heap property
+            std::swap(a[x], a[to]);
+            x = to;
+        }
+    }
+    void push(T x) { a[++sz] = x, up(sz); }
+    void pop() { a[1] = a[sz--], down(1); }
+    T top() { return a[1]; }
+    void build(int n)
+    {
+        sz = n;
+        for (int i = (n >> 1); i; --i) down(i);
+    }
+    void build(T* b, int n)
+    {
+        for (int i = 1; i <= n; ++i) a[i] = b[i];
+        build(n);
+    }
+    bool empty() { return !sz; }
+    void clear() { sz = 0; }
+};
+
+namespace kth
+{
+// the kth largest element in an array
+smallheap<int> S;
+largeheap<int> L;
+void maintain(int k)
+{
+    while (S.sz > k)
+    {
+        L.push(S.top());
+        S.pop();
+    }
+    while (S.sz < k)
+    {
+        S.push(L.top());
+        L.pop();
     }
 }
-inline void push(int x)
+void push(int x, int k)
 {
-    a[++size] = x;
-    up(size);
+    S.push(x);
+    maintain(k);
 }
-inline void del()
+void del(int k)
 {
-    swap(a[size],a[1]);
-    --size;
-    down(1);
+    S.pop();
+    maintain(k);
 }
-int main()
-{
-    read(n);
-    int t,b;
-    while(n--)
-    {
-        read(t);
-        if(t == 1)
-            read(b),push(b);
-        else if(t == 2)
-            printf("%d\n",a[1]);
-        else
-            del();
-    }
-    return 0;
-}
+}  // namespace kth
